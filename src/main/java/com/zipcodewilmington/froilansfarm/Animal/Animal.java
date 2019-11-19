3d26c5766319d2e1dbbfd79a5ba9dd04017d316c
Ryan <ryan@rmiller.dev>
2019-11-19 16:46:31 -0500

    Make directories

---
+++ src/main/java/com/zipcodewilmington/froilansfarm/Animal/Animal.java
@@ -0,0 +1,14 @@
+package com.zipcodewilmington.froilansfarm.Animal;
+
+import com.zipcodewilmington.froilansfarm.NoiseMaker;
+import com.zipcodewilmington.froilansfarm.food.Eater;
+
+public abstract class Animal implements Eater, NoiseMaker {
+    public void mount(){
+
+    }
+
+    public void dismount() {
+
+    }
+}
