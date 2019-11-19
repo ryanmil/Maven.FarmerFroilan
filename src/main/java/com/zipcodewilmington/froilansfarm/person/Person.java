bdecc79fab5de813a277ec151219d63e5fb6386f
Miral Doshi <miraldoshi@Zipoders-MacBook-Pro-2.local>
2019-11-19 18:10:14 -0500

    add list for eat

--- src/main/java/com/zipcodewilmington/froilansfarm/person/Person.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/person/Person.java
@@ -3,10 +3,23 @@ package com.zipcodewilmington.froilansfarm.person;
 import com.zipcodewilmington.froilansfarm.NoiseMaker;
 import com.zipcodewilmington.froilansfarm.Rider;
 import com.zipcodewilmington.froilansfarm.food.Eater;
+import com.zipcodewilmington.froilansfarm.food.Edible;
+
+import java.util.List;
 
 public abstract class Person  implements NoiseMaker, Eater, Rider {
+    List<Edible> foodEat;
+
+    public Person(List<Edible> foodEat) {
+        this.foodEat = foodEat;
+    }
+    public void eat(Edible edible){
+        foodEat.add(edible);
+
+    }
+
     public String makeNoise(String speak) {
-        return null;
+        return "Hey! there ";
     }
     String name;
 
