8c127a264d78685b28437d6543e4fe00218eba0c
Mike King <michkin32@gmail.com>
2019-11-20 13:41:36 -0500

    merge

--- src/test/java/com/zipcodewilmington/froilansfarm/MainApplicationTest.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/MainApplicationTest.java
@@ -1,5 +1,14 @@
 package com.zipcodewilmington.froilansfarm;
 
+import com.zipcodewilmington.froilansfarm.food.Corn;
+import com.zipcodewilmington.froilansfarm.food.Egg;
+import com.zipcodewilmington.froilansfarm.food.Tomato;
+import com.zipcodewilmington.froilansfarm.person.Farmer;
+import com.zipcodewilmington.froilansfarm.person.Person;
+import com.zipcodewilmington.froilansfarm.plants.CornPlant;
+import com.zipcodewilmington.froilansfarm.plants.Crop;
+import com.zipcodewilmington.froilansfarm.plants.TomatoPlant;
+import com.zipcodewilmington.froilansfarm.vehicle.CropDuster;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -8,23 +17,30 @@ import org.junit.Test;
  */
 
 public class MainApplicationTest {
-   /* @Before
+    @Before
     public void setup() {
-       Horse horse = new Horse();
-       Chicken chicken = new Chicken();
-       Farmer froilan = new Farmer();
-       Farmer froilanda = new Farmer();
-       Corn corn = new Corn();
-       Broccoli broccoli = new Broccoli();
-       Egg egg = new Egg();
-       Tomato tomato = new Tomato();
-       CropDuster cropDuster = new CropDuster();
-
-
+        //Instantiation
+        // Farm farm = new Farm();
+        Field field = new Field();
+        CropRow cropRow = new CropRow();
+        CornPlant cornPlant = new CornPlant();
+        TomatoPlant tomatoPlant = new TomatoPlant();
+        Horse horse = new Horse();
+        Chicken chicken = new Chicken();
+        Farmer froilan = new Farmer("Froilan");
+        Farmer froilanda = new Farmer("Froilanda");
+        Corn corn = new Corn();
+        Egg egg = new Egg();
+        Tomato tomato = new Tomato();
+        CropDuster cropDuster = new CropDuster();
+        
+        cropRow.crops.add(cornPlant);
+        field.rows.add();
     }
-    */
+
     @Test
-    public void testTest() {
+    public void SundayTest() {
+
         
     }
 }
